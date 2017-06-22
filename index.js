60664559340c2fa4756cf40d56172272046094f4
Ayan Dalal <adala4@sapient.com>
2017-06-22 14:05:13 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,98 @@
 $(document).ready(function(){
 
-    var db = 'test-graphql';
-    var collection = 'bikes';
-    var apiKey = 'uqqEctpkJziPyUmQJ_McM7Bers0p3rlH';
-    var url = `https://api.mlab.com/api/1/databases/${db}/collections/${collection}?apiKey=${apiKey}`;
+/*************************************GET DATA FROM MLAB SERVER & UPDATE VIEW***************************************/
+
+    var db = 'test-graphql',
+        collection = 'bikes',
+        apiKey = 'uqqEctpkJziPyUmQJ_McM7Bers0p3rlH',
+        url = `https://api.mlab.com/api/1/databases/${db}/collections/${collection}?apiKey=${apiKey}`;
 
     function fetchData(){
 
         $.ajax({
             url: url
-        }).done(appendData);
+        }).done(updateView);
 
     }
 
-    function appendData(data){
-        for(datum in data){
-               console.log(datum);
+    function updateView(data){
+    
+        var showDataDiv = $('#show-data'),
+            heading,
+            addData,
+            datum,
+            showDataDiv_innerHtml;
+
+        heading = `<div class="col-md-12 heading">`;
+        heading+=`<span class="col-md-4">id</span>`;
+        heading+=`<span class="col-md-4">name</span>`;
+        heading+=`<span class="col-md-4">company</span>`;
+        heading+=`</div>`;
+
+        for(var key in data){
+            if(key=='_id'){
+                break;
+            }
+            datum = data[key];
+
+            addData= `<div class="col-md-12 datum">`;
+            addData+=`<span class="col-md-4">${datum.id}</span>`;
+            addData+=`<span class="col-md-4">${datum.name}</span>`;
+            addData+=`<span class="col-md-4">${datum.company}</span>`;
+            addData+=`<button type="submit" class="btn btn-danger delete-btn">Delete</button>`;
+            addData+=`</div>`;
+            heading+=addData;
+            
         }
-     
-        var add = `<div class="col-md-12 datum">`;
+        
+        
+        showDataDiv_innerHtml = showDataDiv.html();
+        showDataDiv_innerHtml+=heading;
+        showDataDiv.html(showDataDiv_innerHtml);
     }
 
     fetchData();
+
+
+
+    /*************************************POST DATA TO MLAB SERVER***************************************/
+
+    var addBtn = $('#add-btn');
+
+    addBtn.click(grabData);
+
+    function grabData(e){
+
+        e.preventDefault();
+
+        var formData = $('#insert-data input');
+
+        postData(formData);
+    }
+
+    function postData(data){
+
+        $.ajax({
+            url: url,
+            data: JSON.stringify({
+                "id": $(data[0]).val(),
+                "name": $(data[1]).val(),
+                "company": $(data[2]).val()
+            }),
+            type: 'POST',
+            contentType: 'application/json',
+            success: successHandler,
+            error: errorHandler 
+        });
+    }
+
+    function successHandler(data){
+   
+        console.log('successfully posted data : ',data);
+        location.reload();
+    }
+
+    function errorHandler(err){
+        console.log('Sorry! ',err);
+    }
 });
